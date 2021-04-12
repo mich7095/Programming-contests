d5915f16f3b9bdf33a63c3dffc9e1df12d60c680
Michel-Corona <michel.carmia@gmail.com>
2021-04-11 19:18:17 -0500

    And range numbers with java - bitwise practice

---
+++ InterviewTraining/BitwiseAnd.java
@@ -0,0 +1,16 @@
+/**
+ * Leetcode 201.
+ * Given two integers left and right that represent the range [left, right], 
+ * return the bitwise AND of all numbers in this range, inclusive.
+ */
+class Solution {
+    public int rangeBitwiseAnd(int left, int right) {
+        int i = 0;
+            while (left != right) {
+                left >>= 1;
+                right >>= 1;
+                i++;
+            }
+            return (right << i);
+    }
+}
